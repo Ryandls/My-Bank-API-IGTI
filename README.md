2beeb4da3d473fcc92a1e9d5e95bbbd21c2aaca1
Bryan Douglas Lima Dos Santos <bryandouglasxd@gmail.com>
2021-03-20 16:13:16 -0300

    Finished

---
+++ README.md
@@ -0,0 +1,27 @@
+<img alt="Gif-App" class="img" title="Na gravação do Gif, a imagem veio bugada por conta do aplicativo de gravação." src="./img/API.gif"/>
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Objetivo da API</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Essa API foi desenvolvido com as seguintes tecnologias:
+
+- JavaScript
+- Node.js
+- Utilizando as bibliotecas: Express, Winston, File System,Cors e Swagger-Ui-Express.
+  -Insomnia - para teste de Request HTTP.
+
+## 🔖 Objetivo da API
+
+Essa API tem como finalidade praticar o conteúdo que foi dado no Módulo 2 do Bootcamp da IGTI, como a utilização das bibliotecas citadas acima, tratamentos de erros, gravação de logs, validação de campos e como fazer documentação da API utilizando o site https://editor.swagger.io.
+
+<div align="center" top="1rem">
+
+<h3><strong>Editado por Bryan</strong><h3>
+<img  src="./img/giphy.gif" width="100px" height="51"/>
+</div>
--- routes/accounts.js
+++ routes/accounts.js
@@ -1,52 +1,65 @@
 import express from 'express';
 import { promises as fs } from 'fs';
-
+import cors from 'cors';
 const { readFile, writeFile } = fs;
 
 const router = express.Router();
 
 //POST add
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     let account = req.body;
+
+    if (!account.name || account.balance == null) {
+      throw new Error('Name e Balance são Obrigatórios');
+    }
+
     const data = JSON.parse(await readFile(global.fileName));
 
-    account = { id: data.nextID++, ...account };
+    account = {
+      id: data.nextID++,
+      name: account.name,
+      balance: account.balance,
+    };
     data.accounts.push(account);
 
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
 
     res.send(account);
+
+    logger.info(`POST /account - ${JSON.stringify(account)}`);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
 //get for all
-router.get('/', async (req, res) => {
+router.get('/', cors(), async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.fileName));
     delete data.nextID;
     res.send(data);
+    logger.info('GET /account');
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 //get by id
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.fileName));
     const account = data.accounts.find(
       (account) => account.id === parseInt(req.params.id)
     );
     res.send(account);
+    logger.info('GET /account/:id');
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
 //delete by id
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.fileName));
     data.accounts = data.accounts.filter(
@@ -54,41 +67,65 @@ router.delete('/:id', async (req, res) => {
     );
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
     res.end();
+    logger.info(`DELETE /account/id: ${req.params.id}`);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
 //put update all
-router.put('/', async (req, res) => {
+router.put('/', async (req, res, next) => {
   try {
     const account = req.body;
     const data = JSON.parse(await readFile(global.fileName));
     const index = data.accounts.findIndex((a) => a.id === account.id);
 
-    data.accounts[index] = account;
+    if (!account.id || !account.name || account.balance == null) {
+      throw new Error('Name e Balance são Obrigatórios');
+    }
 
-    await writeFile(global.fileName, JSON.stringify(data));
+    if (index === -1) {
+      throw new Error('Registro não encotrado.');
+    }
+    console.log(index);
+
+    data.accounts[index].name = account.name;
+    data.accounts[index].balance = account.balance;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
     res.send(account);
+    logger.info(`PUT /account - ${JSON.stringify(account)}`);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-//update only balance
-router.patch('/updateBalance', async (req, res) => {
+//patch update only balance
+router.patch('/updateBalance', async (req, res, next) => {
   try {
     const account = req.body;
     const data = JSON.parse(await readFile(global.fileName));
     const index = data.accounts.findIndex((a) => a.id === account.id);
 
+    if (!account.nextID || account.balance == null) {
+      throw new Error('ID e Balance são Obrigatórios');
+    }
+    if (index === -1) {
+      throw new Error('Registro não encotrado.');
+    }
     data.accounts[index].balance = account.balance;
 
-    await writeFile(global.fileName, JSON.stringify(data));
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
     res.send(data.accounts[index]);
+    logger.info(`PATCH /account/updateBalance - ${JSON.stringify(account)}`);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
+});
+
+router.use((err, req, res, next) => {
+  logger.error(`${req.method}${re.baseUrl} - ${err.message}`);
+  res.status(400).send({ error: err.message });
 });
 
 export default router;
