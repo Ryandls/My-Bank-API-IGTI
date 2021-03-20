b95e91e650e0b35da0ef0bff6c35926c3a8adf43
Bryan Douglas Lima Dos Santos <bryandouglasxd@gmail.com>
2021-03-20 08:15:10 -0300

    Lesson 3
    Method GET

--- routes/accounts.js
+++ routes/accounts.js
@@ -8,14 +8,24 @@ const router = express.Router();
 router.post('/', async (req, res) => {
   try {
     let account = req.body;
-    const data = JSON.parse(await readFile('accounts.json'));
+    const data = JSON.parse(await readFile(global.fileName));
 
     account = { id: data.nextID++, ...account };
     data.accounts.push(account);
 
-    await writeFile('accounts.json', JSON.stringify(data, null, 2));
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
 
     res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    delete data.nextID;
+    res.send(data);
   } catch (err) {
     res.status(400).send({ error: err.message });
   }
